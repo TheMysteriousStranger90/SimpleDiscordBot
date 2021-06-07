bc078e07026f031286099a18c5b9269202995ff6
Bohdan Harabadzhyu <bogdan.garabadzhiu@yandex.ru>
2021-06-07 20:19:04 +0300

    Discord Bot

---
+++ SimpleDiscordBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace SimpleDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Bot().MainAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
